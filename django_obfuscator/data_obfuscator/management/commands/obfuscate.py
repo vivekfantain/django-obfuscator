28663054881e392882edb47bef03f6f97ee18c06
kavinatfantain <kavin@fantain.com>
2015-08-24 16:01:52 +0530

    validation and import limited

--- django_obfuscator/data_obfuscator/management/commands/obfuscate.py
+++ django_obfuscator/data_obfuscator/management/commands/obfuscate.py
@@ -1,5 +1,6 @@
-import os
-import csv
+from os import path
+from csv import DictReader
+
 from django.core.management.base import BaseCommand, CommandError
 
 
@@ -12,15 +13,37 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         csv_name = options['csv_name'][0]
 
-        if os.path.exists(csv_name):
-            self.read_csv(csv_name)
+        if path.exists(csv_name):
+            app_model_data = self.read_csv(csv_name)
+            self.process_csv_data(app_model_data)
         else:
             raise CommandError("CSV file not found.")
+
+    def read_csv(self, csv_name):
+        try:
+            csv_rec = self.get_csv_record(csv_name)
+
+            app_model_data = {}
+            while True:
+                try:
+                    key, value = csv_rec.next()
+                    if key in app_model_data:
+                        if value not in app_model_data[key]:
+                            app_model_data[key].append(value)
+                    else:
+                        app_model_data[key] = [value]
+
+                except StopIteration:
+                    break
+
+            return app_model_data
+        except IOError:
+            self.stdout.write("Couldn't read CSV File.")
 
     @staticmethod
     def get_csv_record(csv_name):
         with open(csv_name) as csv_file:
-            reader = csv.DictReader(csv_file)
+            reader = DictReader(csv_file)
 
             for row in reader:
                 key = row['app_name'], row['model_name']
@@ -28,22 +51,7 @@ class Command(BaseCommand):
 
                 yield key, value
 
-    def read_csv(self, csv_name):
-        try:
-            csv_rec = self.get_csv_record(csv_name)
+    def process_csv_data(self, app_model_data):
+        pass
 
-            model_data = {}
-            while True:
-                try:
-                    key, value = csv_rec.next()
-                    if key in model_data:
-                        if value not in model_data[key]:
-                            model_data[key].append(value)
-                    else:
-                        model_data[key] = [value]
 
-                except StopIteration:
-                    break
-
-        except IOError:
-            self.stdout.write("Couldn't read CSV File.")
