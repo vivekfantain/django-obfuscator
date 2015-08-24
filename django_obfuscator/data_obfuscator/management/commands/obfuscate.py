a37c18cc87a0dfc0ce70ef60b8da4186313c40e8
kavinatfantain <kavin@fantain.com>
2015-08-24 17:10:27 +0530

    strip and cmd line param. modified

--- django_obfuscator/data_obfuscator/management/commands/obfuscate.py
+++ django_obfuscator/data_obfuscator/management/commands/obfuscate.py
@@ -1,23 +1,14 @@
 from os import path
 from csv import DictReader
-from optparse import make_option
-
 from django.core.management.base import BaseCommand, CommandError
 
 
 class Command(BaseCommand):
     help = "Obfuscate the model data's"
-    option_list = BaseCommand.option_list + (
-        make_option('--csv',
-                    action='store',
-                    dest='csv_name',
-                    default=None,
-                    help='Input csv file for processing.'),
-    )
 
     def handle(self, *args, **options):
-        if options['csv_name']:
-            csv_name = options['csv_name']
+        if args:
+            csv_name = args[0]
 
             if path.exists(csv_name):
                 app_model_data = self.read_csv(csv_name)
@@ -25,7 +16,18 @@ class Command(BaseCommand):
             else:
                 raise CommandError("CSV file not found.")
         else:
-            raise CommandError("Please enter CSV name with the command.")
+            raise CommandError("Please enter CSV file name as a parameter.")
+
+    @staticmethod
+    def get_csv_record(csv_name):
+        with open(csv_name) as csv_file:
+            reader = DictReader(csv_file)
+
+            for row in reader:
+                key = row['app_name'].strip(), row['model_name'].strip(),
+                value = row['field_name'].strip(), row['operation'].strip(),
+
+                yield key, value
 
     def read_csv(self, csv_name):
         try:
@@ -47,17 +49,6 @@ class Command(BaseCommand):
             return app_model_data
         except IOError:
             self.stdout.write("Couldn't read CSV File.")
-
-    @staticmethod
-    def get_csv_record(csv_name):
-        with open(csv_name) as csv_file:
-            reader = DictReader(csv_file)
-
-            for row in reader:
-                key = row['app_name'], row['model_name']
-                value = row['field_name'], row['operation'],
-
-                yield key, value
 
     def process_csv_data(self, app_model_data):
         pass
