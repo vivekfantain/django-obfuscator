8f72d0e1a7052beaa50aecb2cda4128c4296c496
kavinatfantain <kavin@fantain.com>
2015-08-28 16:53:10 +0530

    setup configured without download_url

--- setup.py
+++ setup.py
@@ -1,1 +1,16 @@
-__author__ = 'kavin'
+from distutils.core import setup
+
+
+desc = "Obfuscate the sensitive information from database table"
+setup(
+    name='django-obfuscator',
+    packages=['django-obfuscator'],
+    version='0.1',
+    description=desc,
+    author='Vivek Venugopalan',
+    author_email='vivek@fantain.com',
+    url='https://github.com/vivekfantain/django-obfuscator',
+    download_url='https://github.com/peterldowns/mypackage/tarball/0.1',
+    keywords=['obfuscate', 'django-obfuscate'],
+    classifiers=[],
+)
