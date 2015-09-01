670fbcbe14c7c9379dbdd1e313bb49c19f85d47e
kavinatfantain <kavin@fantain.com>
2015-09-01 10:40:26 +0530

    supporting files for packing has been configured.

--- setup.py
+++ setup.py
@@ -14,15 +14,18 @@ Copyright  2015, Fantain Sports Private Limited
    limitations under the License.
 """
 
-from distutils.core import setup
+from setuptools import setup
 
 desc = """ Obfuscate sensitive information in production django databases so
 that it can be distributed to various developers."""
 
 setup(
-    name='django-obfuscator',
-    packages=['django-obfuscator'],
-    version='0.1',
+    name='django_obfuscator',
+    packages=['django_obfuscator'],
+    version='0.5',
+
+    include_package_data=True,
+
     description=desc,
     author='Vivek Venugopalan',
     author_email='vivek@fantain.com',
@@ -30,12 +33,12 @@ setup(
     url='https://github.com/vivekfantain/django-obfuscator',
     download_url='https://github.com/vivekfantain/django-obfuscator/tarball/0.1',
     keywords=['obfuscate', 'django-obfuscate'],
-    classifiers=[],
-    entry_points={
-        'console_scripts': []
-    },
-    install_requires=[
-        'csv',
-        'logging',
-    ]
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+
 )
